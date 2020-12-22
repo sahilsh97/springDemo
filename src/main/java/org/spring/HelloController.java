ad6bbcb4969a9bf8989e5eeeb4ccb9921be19052
sahilsh97 <sahilsh7696@gmail.com>
2020-12-22 06:22:04 +0000

    Add files via upload

---
+++ src/main/java/org/spring/HelloController.java
@@ -0,0 +1,13 @@
+package org.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String getData() {
+		return "Its Working";
+	}
+}
